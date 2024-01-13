4dfea41a767ebafaf96a1a5180c3ff39e7a5c449
carlosgl02 <carlosgarcialorente02@gmail.com>
2024-01-13 12:26:35 +0100

    Initial commit

---
+++ src/main/java/com/cglvsspcmanm/practica4calidad/Practica4calidadApplication.java
@@ -0,0 +1,13 @@
+package com.cglvsspcmanm.practica4calidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica4calidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica4calidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cglvsspcmanm/practica4calidad/Practica4calidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cglvsspcmanm.practica4calidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica4calidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
