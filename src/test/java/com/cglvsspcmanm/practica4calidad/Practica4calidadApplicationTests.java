bf63c4aa10ab96e5ac118b4b91f45d0f6ca0d39c
carlosgl02 <carlosgarcialorente02@gmail.com>
2024-01-13 15:24:00 +0100

    a

--- src/test/java/com/cglvsspcmanm/practica4calidad/Practica4calidadApplicationTests.java
+++ src/test/java/com/cglvsspcmanm/practica4calidad/Practica4calidadApplicationTests.java
@@ -1,6 +1,8 @@
 package com.cglvsspcmanm.practica4calidad;
 
-import org.junit.*;
+import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cglvsspcmanm.practica4calidad.clases.Coche;
@@ -12,23 +14,8 @@ class Practica4calidadApplicationTests {
     private Coche cochef;
     private Coche coche2;
     
-    @Before
-    public void setUp() {
-        coche = new Coche("Toyota");
-        cochef = new Coche("Opel");
-    }
-    
 	@Test
 	void contextLoads() {
-		
 	}
-	
-    @Test
-    public void testAssertEquals() {
-        coche.acelerar(50);
-        cochef.acelerar(500);
-        Assert.assertEquals("La velocidad después de acelerar debería ser igual a 50", 50, coche.getVelocidad());
-        Assert.assertEquals("La velocidad después de acelerar debería ser igual a 50", 50, cochef.getVelocidad());
-    }
 
 }
